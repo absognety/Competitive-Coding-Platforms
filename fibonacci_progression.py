32a1d427e2235d3b7d85a93fe001e9509c24c71b
absognety <cvikas10@ms.ds.uhc.com>
2018-12-25 13:14:47 +0530

    added fibonacci series change

--- fibonacci_progression.py
+++ fibonacci_progression.py
@@ -1,11 +1,14 @@
 def fibonacciSeries(a,b,n):
     i=0
+    fib = [a,b]
     while (i<=n):
         c = a+b
         a=b
         b=c
+        fib.append(c)
         print (c)
         i+=1
+    return (fib)
         
 N = int(input())
 a,b=1,1
