d4286be83763822a23f8c4c5707faa791fa1aa0e
absognety <cvikas10@ms.ds.uhc.com>
2019-01-01 09:31:43 +0530

    Data structure stack implemented in python

---
+++ stack_Implementation.py
@@ -0,0 +1,28 @@
+class Stack():
+    
+    def __init__(self,sequence):
+        
+        self._seq = sequence
+        self._length = len(sequence)
+        self.top=len(sequence)-1
+        
+    def push(self,X):
+        if (self.top==self._length-1):
+            print ('Stach Overflow Condition')
+        else:
+            self._seq.append('')
+            self.top+=1
+            self._seq[self.top]=X
+    
+    def pop(self):
+        if (self.top==-1):
+            print ('Stack is empty, Underflow Condition!!')
+        else:
+            self._seq.pop(self.top)
+            self.top-=1
+    
+    def topElement(self):
+        return self._seq[self.top]
+    
+    def size(self):
+        return (self.top+1)
