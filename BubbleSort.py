cafd1063fd8ef59f4ff3bfb3dc04bf19159d6f38
absognety <vikasch.1994@gmail.com>
2019-02-16 15:13:55 +0530

    bubble sort - Hackerearth Algorithms practice

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+n = int(input())
+Array = [0]*n
+temp=0
+for i in range(n):
+    Array[i] = int(input())
+print (Array)
+for k in range(n-1):
+    for m in range(n-k-1):
+        ##swap case
+        if Array[m] > Array[m+1]:
+            temp=Array[m]
+            Array[m] = Array[m+1]
+            Array[m+1]=temp
+        m+=1
+    k+=1
