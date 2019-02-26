0433e71104595c2bbfb37fe717c32e73e25bef94
absognety <vikasch.1994@gmail.com>
2019-02-26 16:33:42 +0530

    d = number of elements by which rotation is done, n = length of array

--- GeeksForGeeks_Arrays_Questions/Program_For_Array_Rotation.py
+++ GeeksForGeeks_Arrays_Questions/Program_For_Array_Rotation.py
@@ -1,8 +1,9 @@
 arr = list(map(int,input().strip().split()))
-n = int(input())
-def rotate(array,n):
-    cutoff_arr = array[0:n]
-    rotated_arr = array[n:] + cutoff_arr
+n = len(arr)
+d = int(input())
+def rotate(array,m):
+    cutoff_arr = array[0:m]
+    rotated_arr = array[m:] + cutoff_arr
     return (rotated_arr)
 
-print (rotate(arr,n))
+print (rotate(arr,d))
