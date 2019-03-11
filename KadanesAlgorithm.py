d2730de03dfbc5048c91af36eeb65f4a41e2525a
absognety <vikasch.1994@gmail.com>
2019-03-11 09:29:32 +0530

    exception handling for negative numbers in an array

--- KadanesAlgorithm.py
+++ KadanesAlgorithm.py
@@ -2,15 +2,17 @@
 ARR = list(map(int,input().strip().split()))
 
 def Kadanes_Algorithm(arr):
-    
-    max_so_far=max_ending_here=0
-    
-    for a in range(len(arr)):
-        max_ending_here = max_ending_here + arr[a]
-        if max_ending_here < 0:
-            max_ending_here = 0
-        if max_so_far < max_ending_here:
-            max_so_far = max_ending_here
-    return (max_so_far)
+    indicator_array = [True if i >=0 else False for i in arr]
+    if True in indicator_array:
+        max_so_far=max_ending_here=0
+        for a in range(len(arr)):
+            max_ending_here = max_ending_here + arr[a]
+            if max_ending_here < 0:
+                max_ending_here = 0
+            if max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+        return (max_so_far)
+    elif False in indicator_array and True not in indicator_array:
+        return (max(arr))
 
 print (Kadanes_Algorithm(ARR))
