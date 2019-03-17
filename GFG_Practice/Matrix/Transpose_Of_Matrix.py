cc7aece769f982d4c7ee3e049a6afbff4a4477e5
absognety <vikasch.1994@gmail.com>
2019-03-17 17:43:18 +0530

    transpose of a matrix without using .T feature

---
+++ GFG_Practice/Matrix/Transpose_Of_Matrix.py
@@ -0,0 +1,23 @@
+def transposeMatrix(matrix,n):
+    numRows = len(matrix)
+    numCols = len(matrix[0])
+    result = ''
+    c = 0
+    for col in range(numCols):
+        for row in range(numRows):
+            result += '' + str(matrix[row][c])
+        c += 1
+    return (' '.join(list(result)))
+    
+if __name__ == '__main__':
+    T = int(input())
+    for I in range(T):
+        n = int(input().strip())
+        arr = list(map(int,input().strip().split()))
+        matrix = [[0 for i in range(n)] for j in range(n)]
+        k=0
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = arr[k]
+                k += 1
+        print (transposeMatrix(matrix,n))
