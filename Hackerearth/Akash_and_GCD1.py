5774be663cbc3f44509152b6358999cb2240a293
absognety <vikasch.1994@gmail.com>
2019-03-24 08:50:42 +0530

    @amzn Developer Hiring challenge - Akash and GCD 1

--- Hackerearth/Akash_and_GCD1.py
+++ Hackerearth/Akash_and_GCD1.py
@@ -1,3 +1,36 @@
+"""
+Akash is interested in a new function F such that,
+
+F(x) = GCD(1, x) + GCD(2, x) + ... + GCD(x, x)
+
+where GCD is the Greatest Common Divisor.
+Now, the problem is quite simple. Given an array A of size N, there are 2 types of queries:
+1. C X Y : Compute the value of F( A[X] ) + F( A[X + 1] ) + F( A[X + 2] ) + .... + F( A[Y] ) (mod 10^9 + 7)
+2. U X Y: Update the element of array A[X] = Y
+
+Input:
+First line of input contain integer N, size of the array.
+Next line contain N space separated integers the elements of A.
+Next line contain integer Q, number of queries.
+Next Q lines contain one of the two queries.
+
+Output:
+For each of the first type of query, output the required sum (mod 10^9 + 7).
+
+Constraints:
+1 <= N <= 106
+1 <= Q <= 105
+1 <= Ai <= 5*105
+
+For Update ,
+1 <= X <= N
+1 <= Y <= 5*105
+
+For Compute ,
+1 <= X <= Y <= N
+
+"""
+
 def GCD(a,b):
     if a == b:
         return (a)
