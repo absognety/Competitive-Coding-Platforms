104abf86350fc660f3c2db27ce3d5031125c39d1
absognety <vikasch.1994@gmail.com>
2019-03-31 19:08:04 +0530

    backtracking start point from current location

--- GFG_Practice/Matrix/Starting_Point.py
+++ GFG_Practice/Matrix/Starting_Point.py
@@ -1,17 +1,11 @@
 def startPoint(X,Y,numPaths,pointsList):
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    currLocation = [X,Y]
+    XCoords = [i[0] for i in pointsList]
+    YCoords = [i[1] for i in pointsList]
+    sumX = sum(XCoords)
+    sumY = sum(YCoords)
+    return (' '.join([str(currLocation[0]-sumX),str(currLocation[1]-sumY)]))
+          
 if __name__ == '__main__':
     T = int(input())
     for tcase in range(T):
@@ -21,3 +15,4 @@ if __name__ == '__main__':
         pointsList = []
         for p in range(0,len(arr),2):
             pointsList.append(arr[p:p+2])
+        print (startPoint(X,Y,numPaths,pointsList))
