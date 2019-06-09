a45171ce0d301dd5be82bf9486169b6ca335fee0
absognety <vikasch.1994@gmail.com>
2019-06-09 11:14:53 +0530

    Depth first search traversal(recursion) of a graph built with python dict

---
+++ GFG_Practice/GraphAlgorithms/pyDFS_recursive.py
@@ -0,0 +1,30 @@
+
+##Graph Data Structure##
+"""
+Using python dictionary to construct a graph.
+1. Keys of a dictionary are used as nodes of a graph.
+2. Values of each and every node are the list containing nodes
+with which edges are shared.
+"""
+
+#import queue
+
+graph ={'a':['c'],'b':['d'], 
+        'c':['e'],'d':['a', 'd'], 
+        'e':['b', 'c']}
+visited_array=[]
+result = []
+def dfs_recursive(G,s,visited_array=[],rs=[]):
+    #print (visited_array)
+    visited_array.append(s)
+    rs.append(visited_array)
+    for w in G[s]:
+        if w not in visited_array:
+            dfs_recursive(G,w,visited_array=visited_array)
+    return (rs[len(rs)-1])
+            
+print (dfs_recursive(graph,'a',visited_array=[],rs=[]))
+print (dfs_recursive(graph,'b',visited_array=[],rs=[]))
+print (dfs_recursive(graph,'c',visited_array=[],rs=[]))
+print (dfs_recursive(graph,'d',visited_array=[],rs=[]))
+print (dfs_recursive(graph,'e',visited_array=[],rs=[]))
