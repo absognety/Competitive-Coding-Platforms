a137060124557bb3c429939b64b1396fd5c69f96
absognety <vikasch.1994@gmail.com>
2019-06-09 19:40:16 +0530

    matrix to connected graph represented - G[V]=E

---
+++ GFG_Practice/GraphAlgorithms/matrix2Graph.py
@@ -0,0 +1,44 @@
+import collections
+def matrix2graph(mlist):
+    ihor=0
+    iver=0
+    G=collections.defaultdict(list)
+    for ihor in range(len(mlist)):
+        for iver in range(len(mlist[0])):
+            if ihor==0 and iver==0:
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor][iver+1],mlist[ihor+1][iver]]
+                
+            elif ihor==0 and iver==len(mlist[0])-1:
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor][iver-1],mlist[ihor+1][iver]]
+                
+            elif ihor==len(mlist)-1 and iver==0:
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor-1][iver],mlist[ihor][iver+1]]
+                
+            elif ihor==len(mlist)-1 and iver==len(mlist[0])-1:
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor][iver-1],mlist[ihor-1][iver]]
+                
+            elif (0 < ihor < len(mlist)-1) and (iver==0):
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor-1][iver],mlist[ihor][iver+1],mlist[ihor+1][iver]]
+                
+            elif (0 < ihor < len(mlist)-1) and (iver==len(mlist[0])-1):
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor-1][iver],mlist[ihor][iver-1],mlist[ihor+1][iver]]
+                
+            elif (ihor==0) and (0 < iver < len(mlist[0])-1):
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor][iver-1],mlist[ihor][iver+1],mlist[ihor+1][iver]]
+                
+            elif (ihor==len(mlist)-1) and (0 < iver < len(mlist[0])-1):
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor-1][iver],mlist[ihor][iver-1],mlist[ihor][iver+1]]
+                
+            elif (0 < ihor < len(mlist)-1) and (0 < iver < len(mlist[0])-1):
+                G['a'+str(ihor)+str(iver)] = [mlist[ihor-1][iver],mlist[ihor][iver-1],mlist[ihor][iver+1],mlist[ihor+1][iver]]
+                
+    return (G)
+            
+                    
+if __name__ == '__main__':
+    N = int(input())
+    matrix_list = []
+    for i in range(N):
+        arr = list(map(int,input().strip().split()))
+        matrix_list.append(arr)
+    print (matrix2graph(matrix_list))
