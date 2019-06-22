015ae10f1bc0fd1fa3b1fe3140e6b240a73e5001
Padma <vikasch.1994@gmail.com>
2019-06-22 13:16:10 +0530

    add handlers for different N,M combination

--- Hackerearth/Fallabella_ReactJS_Hiring_Challenge/Sid_and_his_Stone_game.py
+++ Hackerearth/Fallabella_ReactJS_Hiring_Challenge/Sid_and_his_Stone_game.py
@@ -1,6 +1,18 @@
 def minNumStones(n,m):
-    total = (n*(n+1))//2
-    return (abs(m-total))
+    S = 0
+    for I in range(1,n+1):
+        S += I
+        if S >= m:
+            break
+    A = (n*(n+1))//2
+    if S!=A and m < A:
+        return (abs(m-A))
+    elif S==A and m==A:
+        return (0)
+    elif S==A and m > A:
+        return (abs(m-A))
+    elif S==A and m < A:
+        return (abs(m-A))
 
 if __name__ == '__main__':
     T = int(input())
