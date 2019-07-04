c4f8a64d2c83102c4000371109cb69b2bd816d92
Padma <vikasch.1994@gmail.com>
2019-07-04 15:47:04 +0530

    question,examples and Approach added for C/C++

--- GFG_Publications/GFG_Contributions_Pending/Sub_Sequences_of_String.py
+++ GFG_Publications/GFG_Contributions_Pending/Sub_Sequences_of_String.py
@@ -1,3 +1,29 @@
+"""
+Given a string str, the task is to print all the sub-sequences of str.
+A subsequence is a sequence that can be derived from another sequence by 
+deleting some or no elements without changing the order of the remaining 
+elements.
+
+Examples:
+
+Input: str = “abc”
+Output: a b ab c ac bc abc
+
+Input: str = “geek”
+Output: g e ge e ge ee gee k gk ek gek ek gek eek geek
+
+
+Approach: 
+
+Write a recursive function that prints every sub-sequence of the 
+sub-string starting from the second character str[1, n – 1] after appending 
+the first character of the string str[0] in the beginning of every sub-sequence. 
+Terminating condition will be when the passed string is empty, 
+in that case the function will return an empty arraylist.
+
+"""
+
+
 import itertools
 
 def Sub_Sequences(STR):
