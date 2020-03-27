9bfc4ec12989ffa048544a0db9c882197294f189
absognety <vikasch.1994@gmail.com>
2020-03-27 10:50:52 +0530

    Add problem statement with test cases

--- COVID-19-21-Day-Coding-Streak/Day-1/TriangleAndSquare.py
+++ COVID-19-21-Day-Coding-Streak/Day-1/TriangleAndSquare.py
@@ -1,3 +1,28 @@
+"""
+Given a positive integer B denoting the base of a right angled isosceles triangle. Find the maximum number of squares of size 2×2 units that can fit in it.
+
+Input:
+First line of input contains an integer T denoting number of test cases. For each test case, there exists one line containing integer B ie-base of right angled isosceles triangle.
+
+Output:
+For each test case, print the maximum number of squares that can be fit into the triangle.
+
+Constraints:
+1<= T <=100
+1<= B <= 1000
+
+Example:
+
+Input:
+2
+8
+2
+
+Output:
+6
+0
+"""
+
 def triangleAndSquare(base):
     if base <= 2:
         return 0
