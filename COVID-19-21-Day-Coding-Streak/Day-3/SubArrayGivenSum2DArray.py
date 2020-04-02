c665dbf83ccd20879695d780ef225b729a2d911d
absognety <vikasch.1994@gmail.com>
2020-04-02 14:57:13 +0530

    1.problem statement added 2.function reset

--- COVID-19-21-Day-Coding-Streak/Day-3/SubArrayGivenSum2DArray.py
+++ COVID-19-21-Day-Coding-Streak/Day-3/SubArrayGivenSum2DArray.py
@@ -1,25 +1,59 @@
+"""
+
+Given a square matrix arr[ ][ ] of n rows and an integer x, find out whether 
+there is a sub-array inside the matrix with sum of elements equal to x. 
+The sub-array can be a 2-D array of any number of rows and columns.
+
+Input:
+First line of input consists of number of test cases t. 
+Then t test cases follow. In each test case, first line 
+holds the integer n which represents numbers of rows in square matrix. 
+Second line of each test case consists of n*n space separated integers 
+representing the matrix arr. Third and last line of test case holds the value x.
+
+Output:
+If a sub-array with sum x is found, 'Yes' is printed. Otherwise, 'No' is printed.
+
+Your Task:
+Your task is to complete the function is_rectangle_there( ). 
+This function takes the matrix, n and x as arguments and returns 0 or 1.
+
+Constraints:
+T = 250
+2 <= n <= 50
+1 <= arr[i][j] <= 1000
+1 <= x <= 5000000
+
+Example:
+Input:
+2
+3
+1 4 2 4 2 5 7 5 7
+30
+2
+4 5 9 10
+8
+Output:
+Yes
+No
+Explanation:
+Testcase 1 :
+Input array:
+1 4 2
+4 2 5
+7 5 7
+Sub matrix with sum 30:
+4 2 5
+7 5 7
+
+"""
+
+
 def is_rectangle_there(arr,n,x):
-    arrays_sum = [sum(u) for u in arr]
-    current_sum = 0
-    start = 0
-    end = -1
-    hashMap = dict()
-    for i in range(n):
-        current_sum += arrays_sum[i]
-        if current_sum - x == 0:
-            start = 0
-            end = i
-            break
-        if (current_sum-x) in hashMap:
-            start = hashMap.get(current_sum-x)
-            end = i
-            break
-        hashMap[current_sum] = i
-    if end == -1:
-        return False
-    else:
-        return True
-        
+    """
+    solution goes here
+    """
+    
 
 if __name__ == '__main__':    
     t=int(input())
