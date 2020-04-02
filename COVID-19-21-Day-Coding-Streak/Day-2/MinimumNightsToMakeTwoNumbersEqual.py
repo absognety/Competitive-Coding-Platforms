49cd89e1d2f4d88c8b64bb1d32928cfea25c16cd
absognety <vikasch.1994@gmail.com>
2020-04-02 12:11:57 +0530

    problem description and testcases along with I/O constraints added

--- COVID-19-21-Day-Coding-Streak/Day-2/MinimumNightsToMakeTwoNumbersEqual.py
+++ COVID-19-21-Day-Coding-Streak/Day-2/MinimumNightsToMakeTwoNumbersEqual.py
@@ -1,5 +1,47 @@
 #User function Template for python3
 
+"""
+Xenon has A number of sweets , but his lucky number is B . Everynight he 
+takes a decision to eat a sweet or not. If he chooses to eat , then his 
+number of sweets will get decreased by one and if he chooses not to eat , 
+then number of sweets gets doubled. Xenon wants to make his number of sweets 
+exactly equal to B in minimum number of nights. You have to print his optimal 
+decision for Everynight . If he eats a sweet at ith night then print "eat" 
+otherwise print "overflow" .
+
+Input:
+First line of input contains a positive integer T denoting number of 
+test cases . For each test case , first line contains two space seperated 
+positive integers A (initial number of sweets) and B (lucky number) . 
+
+Output:
+For each test case , print the sequence of "eat" and "overflow" until 
+number of sweets becomes exactly equal to B
+
+Constraints:
+1 <= T <= 100
+1 <=  A , B <= 10000
+
+Your Task:
+This is a funcional problem , you don't need to take input . Complete 
+the function getDecision() which accepts two positive integers A and B as 
+parameters and returns a vector of strings.
+
+Example:
+Input:
+2
+5 8
+1 9
+Output:
+eat overflow
+overflow overflow eat overflow eat overflow eat
+
+Explanation:
+Testcase 1: The fastest way to reach 8 from 5 would be 5-4-8 .
+Testcase 2: The fastest way to reach 9 from 1 would be 1-2-4-3-6-5-10-9.
+
+"""
+
 def getPath(a, b):
     # the bfs queue
     queue = []
@@ -22,7 +64,7 @@ def getPath(a, b):
 
         # add an element to the queue (if it was not
         # already visited
-        def try_add(next_val):
+        def try_ops(next_val):
             if next_val in length:
                 return
             queue.append(next_val)
@@ -30,9 +72,9 @@ def getPath(a, b):
             previous[next_val] = val
         
         # add the allowed operations here
-        try_add(val - 1)
+        try_ops(val - 1)
 
-        try_add(val * 2)
+        try_ops(val * 2)
 
         # check whether we already have a solution
         if b in length:
