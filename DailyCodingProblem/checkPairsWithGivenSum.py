9e23fc60821c986fb8aae1acb6ece7958b80b498
absognety <vikasch.1994@gmail.com>
2020-04-18 19:28:51 +0530

    problem statement added to code file

--- DailyCodingProblem/checkPairsWithGivenSum.py
+++ DailyCodingProblem/checkPairsWithGivenSum.py
@@ -1,3 +1,13 @@
+"""
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the 
+list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+"""
+
+
 def isPairWithGivenSum(arr,n,x):
     left,right = 0,n-1
     arr = sorted(arr)
