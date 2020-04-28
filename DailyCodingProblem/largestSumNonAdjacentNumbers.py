ee26fc324b07c76c23aac5206133582068ea5c02
absognety <vikasch.1994@gmail.com>
2020-04-28 13:40:15 +0530

    largest sum of non-adjacent numbers with 0 and negative numbers possible using itertools

--- DailyCodingProblem/largestSumNonAdjacentNumbers.py
+++ DailyCodingProblem/largestSumNonAdjacentNumbers.py
@@ -12,3 +12,58 @@ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5.
 Follow-up: Can you do this in O(N) time and constant space?
 
 """
+import itertools
+import math
+def largestSum(arr,n):
+    evens = arr[0:n:2]
+    odds = arr[1:n:2]
+    maxSum1 = -1 * math.inf
+    maxSum2 = -1 * math.inf
+    for i in range(1,len(evens)+1):
+        combinations = list(itertools.combinations(evens,i))
+        for c in combinations:
+            if sum(c) > maxSum1:
+                maxSum1 = sum(c)
+    for i in range(1,len(odds)+1):
+        combinations = list(itertools.combinations(odds,i))
+        for c in combinations:
+            if sum(c) > maxSum2:
+                maxSum2 = sum(c)
+    i = 0
+    maxSum3 = -1 * math.inf
+    while i < len(evens):
+        j = i + 1
+        while (j < len(odds)):
+            new_list = [evens[i]]+odds[j:]
+            print (new_list)
+            for k in range(1,len(new_list)+1):
+                combinations = list(itertools.combinations(new_list,k))
+                for c in combinations:
+                    if sum(c) > maxSum3:
+                        maxSum3 = sum(c)
+            j += 1
+        i += 1
+    
+    j = 0
+    maxSum4 = -1 * math.inf
+    while j < len(odds):
+        i = j + 2
+        while (i < len(evens)):
+            new_list = [odds[j]]+evens[i:]
+            print (new_list)
+            for k in range(1,len(new_list)+1):
+                combinations = list(itertools.combinations(new_list,k))
+                for c in combinations:
+                    if sum(c) > maxSum4:
+                        maxSum4 = sum(c)
+            i += 1
+        j += 1
+    return max(maxSum1,maxSum2,maxSum3,maxSum4)
+
+    
+if __name__ == '__main__':
+    T = int(input())
+    for tcs in range(T):
+        arr = list(map(int,input().strip().split()))
+        n = len(arr)
+        print (largestSum(arr,n))
