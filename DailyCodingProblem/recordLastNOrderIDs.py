79c8dfecc48d7bacd2b03c9a02a2e59891cf6a81
absognety <vikasch.1994@gmail.com>
2020-05-03 09:53:55 +0530

    record last N order IDs of e-commerce website transactions

---
+++ DailyCodingProblem/recordLastNOrderIDs.py
@@ -0,0 +1,12 @@
+"""
+Daily Coding Problem #16
+This problem was asked by Twitter.
+
+You run an e-commerce website and want to record the last N order ids in a log. 
+Implement a data structure to accomplish this, with the following API:
+    1. record(order_id): adds the order_id to the log
+    2. get_last(i): gets the ith last element from the log. i is guaranteed to be smaller 
+    than or equal to N.
+    3. You should be as efficient with time and space as possible.
+    
+"""
