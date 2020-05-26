12c70cf10e683e4d173730f8d75809430367bf71
absognety <vikasch.1994@gmail.com>
2020-05-26 08:49:28 +0530

    return possible resconstruction of sentence if possible

--- DailyCodingProblem/problem_22.py
+++ DailyCodingProblem/problem_22.py
@@ -17,3 +17,33 @@ string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 ['bedbath', 'and', 'beyond'].
 
 """
+import re
+import operator
+def unique_by_key(elements, key=None):
+    if key is None:
+        key = lambda e: e
+    return {key(el): el for el in elements}.values()
+
+def construction(string,words):
+    result = []
+    ismissing = []
+    for word in words:
+        if re.search(word,string):
+            result.append((word,re.search(word,string).span()[0]))
+        else:
+            ismissing.append(1)
+    if ismissing:
+        return 'null'
+    result = unique_by_key(result,key=operator.itemgetter(1))
+    result = sorted(result,key=lambda x: x[1])
+    result = [d[0] for d in result]
+    return result
+
+if __name__ == '__main__':
+    words = {'quick','brown','the','fox'}
+    string = 'thequickbrownfox'
+    print (construction(string,words))
+    
+    words2 = {'bed','bath','bedbath','and','beyond'}
+    string2 = 'bedbathandbeyond'
+    print (construction(string2,words2))
