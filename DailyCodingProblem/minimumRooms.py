aadda2cd1955e0790acf5f0972cc5360bd3748d7
absognety <vikasch.1994@gmail.com>
2020-05-30 18:10:59 +0530

    Added heap implementation in python

--- DailyCodingProblem/minimumRooms.py
+++ DailyCodingProblem/minimumRooms.py
@@ -11,7 +11,10 @@ return 2.
 
 """
 import itertools
+from heapq import heappush,heappop
 def min_rooms(intervals):
+    if len(intervals) == 1:
+        return 1
     pairs = list(itertools.combinations(intervals,2))
     minrooms = 0
     for tup in pairs:
@@ -21,6 +24,37 @@ def min_rooms(intervals):
             minrooms += 1
     return minrooms
 
+
+def minimum_rooms(intervals):
+    if not intervals:
+        return 0
+    sorted_intervals = sorted(intervals,key=lambda it: (it[0],it[1]))
+    cnt = 0
+    heap = []
+    #print ([(i[0],i[1]) for i in sorted_intervals])
+    for tup in sorted_intervals:
+        start,end = tup[0],tup[1]
+        
+        while heap and (heap[0] <= start):
+            heappop(heap)
+            
+        heappush(heap,end)
+        cnt = max(len(heap),cnt)
+        #print (start,end,heap)
+    return cnt
+
 if __name__ == '__main__':
     intervals = [(30, 75), (0, 50), (60, 150)]
+    intervals1 = [(0,30),(5,10),(15,20)]
+    intervals2 = [(2,7)]
+    intervals3 = [(10,30),(5,10),(5,6),(15,20)]
+    
+    print (min_rooms(intervals1))
+    print (min_rooms(intervals2))
+    print (min_rooms(intervals3))
     print (min_rooms(intervals))
+    
+    print (minimum_rooms(intervals1))
+    print (minimum_rooms(intervals2))
+    print (minimum_rooms(intervals3))
+    print (minimum_rooms(intervals))
