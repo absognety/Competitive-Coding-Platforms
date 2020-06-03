d2637f54d21d98eb53bd9619eb7202ea5c83a2fe
absognety <vikasch.1994@gmail.com>
2020-06-03 22:15:56 +0530

    Add solution using collections API (HashMap)

--- interviewing.io/Salesforce/findTheElementOccurringOnce.py
+++ interviewing.io/Salesforce/findTheElementOccurringOnce.py
@@ -4,4 +4,22 @@ the array except for one element which occurs only once.
 
 Find that element
 
+Two solutions:
+    1. Brute force approach (looping over the whole array and perform extensive search)
+    2. Use a hashmap (store the frequencies of all the elements)
+
 """
+
+import collections
+def findThatElement(array):
+    n = len(array)
+    if n%2 != 1:
+        return "no element is occurring once"
+    freqs = collections.Counter(array)
+    for k,v in freqs.items():
+        if v == 1:
+            return k
+        
+if __name__ == '__main__':
+    array = [1,1,2]
+    print (findThatElement(array))
