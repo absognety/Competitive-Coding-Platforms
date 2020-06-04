ce852bdd4ec98561d006d6212d500e42ccb262c4
absognety <vikasch.1994@gmail.com>
2020-06-04 09:41:23 +0530

    Minimum number of elements required from the array to get given number

---
+++ interviewing.io/Salesforce/minNumbers.py
@@ -0,0 +1,12 @@
+"""
+Given dollar_bills = [1,5,10,50,100] and given a value, what is the minumum
+number of dollar bills we need to make that number (Assume operation sum)
+
+Example:
+    x = 105
+    x = 100 + 5 (minimum number of dollar bills = 2)
+    
+Generalized:
+    Given a number and array of numbers, what is the minumum number of elements
+    from array needed such that their sum results in the given number.
+"""
