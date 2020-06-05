9dcd944d6719b97bf0a2eafd3286fd0ae06f6fb3
absognety <vikasch.1994@gmail.com>
2020-06-05 12:07:58 +0530

    Added greedy implementation

--- interviewing.io/Salesforce/minNumbers.py
+++ interviewing.io/Salesforce/minNumbers.py
@@ -18,6 +18,8 @@ Solution:
             x = 173
             x = 100 + 50 + 10 + 10 + 1 + 1 + 1
             Here 10 is used twice and 1 thrice.
+    Approach-2:
+        2. greedy solution
 """
 
 import itertools
@@ -30,11 +32,25 @@ def minimum_numbers(arr,k):
                 return len(c)
     return "Given value cannot be achieved with given array elements"
 
+
+def minimum_numbers2(arr,k):
+    result = 0
+    arr = sorted(arr,reverse=True)
+    while k > 0:
+        for a in arr:
+            if a <= k:
+                k -= a
+                result += 1
+                break
+    return result
+
 if __name__ == '__main__':
     dollar_bills = [1,5,10,50,100]
     x = 105
     print (minimum_numbers(dollar_bills,x))
+    print (minimum_numbers2(dollar_bills,x))
     
     arr = [2,4,10,-2,2]
     k = 4
     print (minimum_numbers(arr,k))
+    print (minimum_numbers2(arr,k))
