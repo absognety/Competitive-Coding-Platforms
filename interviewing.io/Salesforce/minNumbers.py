117c9cdb0e1d2671a76b41abd260197dd830cc0f
absognety <vikasch.1994@gmail.com>
2020-06-05 12:19:44 +0530

    Added modulo solution for the same

--- interviewing.io/Salesforce/minNumbers.py
+++ interviewing.io/Salesforce/minNumbers.py
@@ -20,17 +20,20 @@ Solution:
             Here 10 is used twice and 1 thrice.
     Approach-2:
         2. greedy solution
+        
+    Approach-3:
+        3. Modulo-operation (modification of Approach-2)
 """
 
-import itertools
-def minimum_numbers(arr,k):
-    #duplication is restricted here
-    for i in range(1,len(arr)):
-        cbs = set(itertools.combinations(arr,i))
-        for c in cbs:
-            if sum(c) == k:
-                return len(c)
-    return "Given value cannot be achieved with given array elements"
+#import itertools
+#def minimum_numbers(arr,k):
+#    #duplication is restricted here
+#    for i in range(1,len(arr)):
+#        cbs = set(itertools.combinations(arr,i))
+#        for c in cbs:
+#            if sum(c) == k:
+#                return len(c)
+#    return "Given value cannot be achieved with given array elements"
 
 
 def minimum_numbers2(arr,k):
@@ -44,13 +47,23 @@ def minimum_numbers2(arr,k):
                 break
     return result
 
+def minimum_numbers3(arr,k):
+    result = 0
+    arr = sorted(arr,reverse=True)
+    for a in arr:
+        result += (k//a)
+        k = k%a
+    return result
+
 if __name__ == '__main__':
     dollar_bills = [1,5,10,50,100]
     x = 105
-    print (minimum_numbers(dollar_bills,x))
+    #print (minimum_numbers(dollar_bills,x))
     print (minimum_numbers2(dollar_bills,x))
+    print (minimum_numbers3(dollar_bills,x))
     
     arr = [2,4,10,-2,2]
     k = 4
-    print (minimum_numbers(arr,k))
+    #print (minimum_numbers(arr,k))
     print (minimum_numbers2(arr,k))
+    print (minimum_numbers3(arr,k))
