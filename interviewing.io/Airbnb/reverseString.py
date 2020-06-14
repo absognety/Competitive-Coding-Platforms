2c75d0c0f8e6f46f82f504778abba42e3170169d
absognety <vikasch.1994@gmail.com>
2020-06-14 21:01:51 +0530

    3 different functions that reverses the string

--- interviewing.io/Airbnb/reverseString.py
+++ interviewing.io/Airbnb/reverseString.py
@@ -2,3 +2,29 @@
 Write a quick function that reverses a given string
 
 """
+
+
+#Index based method
+def reverse_string(s):
+    return s[::-1]
+
+#Using built-in reverse method on class list
+def reverse_string1(s):
+    h = list(s)
+    h.reverse()
+    return "".join(h)
+
+
+#Using O(n) auxiliary space
+def reverse_string2(s):
+    v = len(s)
+    u = [''] * v
+    for c in range(v):
+        u[v-c-1] = s[c]
+    return ''.join(u)
+
+if __name__ == '__main__':
+    string = "competitive"
+    print (reverse_string(string))
+    print (reverse_string1(string))
+    print (reverse_string2(string))
