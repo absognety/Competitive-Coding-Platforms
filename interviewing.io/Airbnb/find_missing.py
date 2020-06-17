51dde7fccb02b0ce8816d119996da53459211c9c
absognety <vikasch.1994@gmail.com>
2020-06-17 09:32:22 +0530

    1. assert statment to validate the result 2. driver code to test the results

--- interviewing.io/Airbnb/find_missing.py
+++ interviewing.io/Airbnb/find_missing.py
@@ -10,5 +10,11 @@ Example:
 #Using set() built-in function
 def findMissing(arr1,arr2):
     res = set(arr1) - set(arr2)
+    assert len(res) == 1,"there is more than one missing element"
     return list(res)[0]
 
+
+if __name__ == '__main__':
+    arr1 = [4,8,12,9,3]
+    arr2 = [4,8,9,3]
+    print (findMissing(arr1,arr2))
