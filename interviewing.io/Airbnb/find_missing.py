69b66c586cd5340a6c407279d1075010af0ca3b0
absognety <vikasch.1994@gmail.com>
2020-06-17 09:41:32 +0530

    using set as lookup

--- interviewing.io/Airbnb/find_missing.py
+++ interviewing.io/Airbnb/find_missing.py
@@ -14,7 +14,19 @@ def findMissing(arr1,arr2):
     return list(res)[0]
 
 
+#Using set() (O(N) and O(N) space)
+def findMissing1(arr1,arr2):
+    elems = set()
+    for s in arr2:
+        elems.add(s)
+    for b in arr1:
+        if b not in elems:
+            return b
+    return -1
+    
+
 if __name__ == '__main__':
     arr1 = [4,8,12,9,3]
     arr2 = [4,8,9,3]
     print (findMissing(arr1,arr2))
+    print (findMissing1(arr1,arr2))
