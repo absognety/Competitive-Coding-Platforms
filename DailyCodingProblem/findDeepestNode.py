f670d69fdbd83d31685f2e8588be456ee285323d
absognety <vikasch.1994@gmail.com>
2020-07-05 20:43:56 +0530

    find deepest node in a binary tree

---
+++ DailyCodingProblem/findDeepestNode.py
@@ -0,0 +1,14 @@
+"""
+Daily Coding Problem # 80
+
+This problem was asked by Google.
+
+Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
+
+"""
