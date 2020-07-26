d9c8c503029d01f0f1c28e40aa74b994d71ca8f7
absognety <vikasch.1994@gmail.com>
2020-07-26 22:00:14 +0530

    1.Handle case when there is no bigger element on right side exists 2.Add another problem model where next bigger element on the right side of every element is minimum

--- Collabedit/next_greater_element.py
+++ Collabedit/next_greater_element.py
@@ -13,12 +13,15 @@ Output: [12,7,11,11,12,-1]
 
 """
 
-#Brute force solution
+#first bigger element on the right side of every element
 def nextGreaterElement(arr):
     result = []
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         ele = arr[i]
         arr_n = arr[(i+1):]
+        if max(arr_n) <= ele:
+            result.append(-1)
+            continue
         m = 0
         while (m <= len(arr_n)-1):
             if arr_n[m] > ele:
@@ -30,10 +33,29 @@ def nextGreaterElement(arr):
     return result
 
 
-#Optimal solution
+#minimum first bigger element on the right side of every element
+def min_next_greater_element(arr):
+    result = []
+    for i in range(len(arr)-1):
+        ele = arr[i]
+        arr_n = arr[(i+1):]
+        if max(arr_n) <= ele:
+            result.append(-1)
+            continue
+        tmp = sorted(arr_n)
+        m = 0
+        while (m <= len(tmp)-1):
+            if tmp[m] > ele:
+                result.append(tmp[m])
+                break
+            else:
+                m += 1
+    result.append(-1)
+    return result
 
 
 if __name__ == '__main__':
     for tcase in range(T:=int(input())):
         arr = list(map(int,input().strip().split()))
         print (nextGreaterElement(arr))
+        print (min_next_greater_element(arr))
