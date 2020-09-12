10f5e28e12ac4baa0d00e16a6f66fc83dd14a106
absognety <vikasch.1994@gmail.com>
2020-09-12 20:52:23 +0530

    Extend the function to handle more scenaries for p and q indices

--- DailyCodingProblem/sumSubList.py
+++ DailyCodingProblem/sumSubList.py
@@ -15,7 +15,16 @@ optimized over the pre-processing step.
 """
 
 def sublist_sum(arr,p,q):
-    assert (p>=0) & (q<=len(arr)-1),"Given indices are not valid"
+    if q is None:
+        assert abs(p) <= len(arr)-1,"Given first index for slicing is not \
+                                     valid"
+        return sum(arr[p:])
+    if p is None:
+        assert abs(q) <= len(arr)-1,"Given second index for slicing is not \
+                                     valid"
+        return sum(arr[:q])
+    cond = (abs(p) <= len(arr)-1) & (abs(q) <= len(arr)-1)
+    assert cond,"Given indices are not valid"
     return sum(arr[p:q])
 
 print (sublist_sum([1,2,3,4,5],1,3))
