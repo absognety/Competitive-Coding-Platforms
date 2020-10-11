7fbe8d224535e6f21251b78588be1c1f1609d6b4
absognety <vikasch.1994@gmail.com>
2020-10-11 22:49:27 +0530

    Problem #153

---
+++ DailyCodingProblem/DistanceBetween2Words.py
@@ -0,0 +1,13 @@
+"""
+Daily Coding Problem #153
+
+Find an efficient algorithm to find the smallest distance 
+(measured in number of words) between any two given words in a 
+string.
+
+For example, given words "hello", and "world" and a text 
+content of "dog cat hello cat dog dog hello cat world", 
+return 1 because there's only one word "cat" in between the 
+two words.
+
+"""
