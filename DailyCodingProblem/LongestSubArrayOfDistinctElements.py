f6fc0dfad4ed2b3a04b5f23df672cf44f0555488
Vikas Chitturi <vikasch.1994@gmail.com>
2020-10-25 18:58:36 +0530

    Using itertools ~ find the longest subarray length with distinct elements

--- DailyCodingProblem/LongestSubArrayOfDistinctElements.py
+++ DailyCodingProblem/LongestSubArrayOfDistinctElements.py
@@ -10,3 +10,19 @@ For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5
 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 
 """
+
+###Using itertools
+import itertools
+def longest_subarray_distinct(arr):
+    if len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return len(arr)
+    for l in range(len(arr),0,-1):
+        subarrays = set(itertools.combinations(arr, l))
+        for suba in subarrays:
+            if len(suba) == len(set(suba)):
+                return len(suba)
+    return 0
+
+print (longest_subarray_distinct([5, 1, 3, 5, 2, 3, 4, 1]))
