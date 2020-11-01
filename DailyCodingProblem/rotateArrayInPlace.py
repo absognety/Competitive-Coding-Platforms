acefed7dff671234a0b52c06b0f1a51ffe267edb
absognety <vikasch.1994@gmail.com>
2020-11-01 21:43:22 +0530

    remove whitespace

--- DailyCodingProblem/rotateArrayInPlace.py
+++ DailyCodingProblem/rotateArrayInPlace.py
@@ -3,7 +3,7 @@ Daily Coding Problem #197
 
 This problem was asked by Amazon.
 
-Given an array and a number k that's smaller than the length of the 
+Given an array and a number k that's smaller than the length of the
 array, rotate the array to the right k elements in-place.
 
 """
