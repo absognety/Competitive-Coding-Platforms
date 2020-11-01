cd9b5bb8d97bdbd92a7ab5e6839f7be504be1ee0
absognety <vikasch.1994@gmail.com>
2020-11-01 21:42:42 +0530

    Problem #197: rotate array by k elements to the right in place

---
+++ DailyCodingProblem/rotateArrayInPlace.py
@@ -0,0 +1,18 @@
+"""
+Daily Coding Problem #197
+
+This problem was asked by Amazon.
+
+Given an array and a number k that's smaller than the length of the 
+array, rotate the array to the right k elements in-place.
+
+"""
+
+def rotate_inplace(arr,k):
+    i = 1
+    while (i <= k):
+        arr.append(arr[i-1])
+        i += 1
+    return arr[k:]
+
+print (rotate_inplace([1,2,3,4,5,5,6],4))
