a31762ad75698ee24a7176266ab3a408c826fdfa
absognety <vikasch.1994@gmail.com>
2020-11-09 17:31:46 +0530

    check whether given integer is palindrome without converting it into string

---
+++ DailyCodingProblem/isIntegerPalindrome.py
@@ -0,0 +1,26 @@
+"""
+Daily Coding Problem #202
+
+This problem was asked by Palantir.
+
+Write a program that checks whether an integer is a palindrome. 
+For example, 121 is a palindrome, as well as 888. 678 is not a 
+palindrome. Do not convert the integer into a string.
+
+"""
+
+def isIntegerPalindrome(num):
+    digits = []
+    if (num%10) == 0:
+        return False
+    while (num%10 != 0):
+        digits.append(num%10)
+        num = num//10
+    if digits == digits[::-1]:
+        return True
+    return False
+
+print (isIntegerPalindrome(888))
+print (isIntegerPalindrome(121))
+print (isIntegerPalindrome(678))
+print (isIntegerPalindrome(234432))
