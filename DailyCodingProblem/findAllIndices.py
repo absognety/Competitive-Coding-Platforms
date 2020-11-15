27bfb0d49944a42d10008221d3d0b1c8443f15bb
absognety <vikasch.1994@gmail.com>
2020-11-15 17:33:42 +0530

    find all indices of pattern in a string using regular expressions

--- DailyCodingProblem/findAllIndices.py
+++ DailyCodingProblem/findAllIndices.py
@@ -8,3 +8,18 @@ occurrences of the pattern in the string. For example, given the string
 "abracadabra" and the pattern "abr", you should return [0, 7].
 
 """
+
+import re
+def find_pattern(pattern,string):
+    indices = []
+    for m in re.finditer(pattern,string):
+        indices.append(m.span()[0])
+    return indices
+
+if __name__ == '__main__':
+    pattern = "abr"
+    string = "abracadabra"
+    print (find_pattern(pattern,string))
+    pattern = "eee"
+    string = "bsbsnsnnsqeiieee@$ghhg88888****eeednjdjskan!!!!^%&"
+    print (find_pattern(pattern,string))
