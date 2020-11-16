b1e9f3cec8a5c93635e25727e8c3174c75c16532
absognety <vikasch.1994@gmail.com>
2020-11-16 13:13:36 +0530

    Formatting and indentation fixes

--- DailyCodingProblem/generateValidIPCombinations.py
+++ DailyCodingProblem/generateValidIPCombinations.py
@@ -19,50 +19,39 @@ For example, given "2542540123", you should return
 
 # Function checks whether IP digits 
 # are valid or not. 
-def is_valid(ip): 
-
-	# Splitting by "." 
-	ip = ip.split(".") 
-	
-	# Checking for the corner cases 
-	for i in ip: 
-		if (len(i) > 3 or int(i) < 0 or
-						int(i) > 255): 
-			return False
-		if len(i) > 1 and int(i) == 0: 
-			return False
-		if (len(i) > 1 and int(i) != 0 and
-			i[0] == '0'): 
-			return False
-			
-	return True
+def is_valid(ip):
+    # Splitting by "."
+    ip = ip.split(".")
+    # Checking for the corner cases
+    for i in ip:
+        if (len(i) > 3 or int(i) < 0 or int(i) > 255):
+            return False
+        if len(i) > 1 and int(i) == 0:
+            return False
+        if (len(i) > 1 and int(i) != 0 and i[0] == '0'):
+            return False
+    return True
 
 # Function converts string to IP address 
-def convert(s): 
-	
-	sz = len(s) 
-
-	# Check for string size 
-	if sz > 12: 
-		return [] 
-	snew = s 
-	l = [] 
-
-	# Generating different combinations. 
-	for i in range(1, sz - 2): 
-		for j in range(i + 1, sz - 1): 
-			for k in range(j + 1, sz): 
-				snew = snew[:k] + "." + snew[k:] 
-				snew = snew[:j] + "." + snew[j:] 
-				snew = snew[:i] + "." + snew[i:] 
-				
-				# Check for the validity of combination 
-				if is_valid(snew): 
-					l.append(snew) 
-					
-				snew = s 
-				
-	return l 
+def convert(s):
+    sz = len(s) 
+    # Check for string size
+    if sz > 12:
+        return []
+    snew = s
+    l = []
+	# Generating different combinations.
+    for i in range(1, sz - 2):
+        for j in range(i + 1, sz - 1):
+            for k in range(j + 1, sz):
+                snew = snew[:k] + "." + snew[k:]
+                snew = snew[:j] + "." + snew[j:]
+                snew = snew[:i] + "." + snew[i:]
+                # Check for the validity of combination
+                if is_valid(snew):
+                    l.append(snew)
+                snew = s
+    return l 
 
 # Driver code		 
 A = "25525511135"
