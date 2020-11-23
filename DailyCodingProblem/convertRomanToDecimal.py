354bdc81c18eced162680e0b6705a6f2b39c5bee
absognety <vikasch.1994@gmail.com>
2020-11-23 12:55:25 +0530

    Convert roman to decimal for numbers

--- DailyCodingProblem/convertRomanToDecimal.py
+++ DailyCodingProblem/convertRomanToDecimal.py
@@ -24,3 +24,28 @@ For the input XIV, for instance, you should return 14.
 
 
 """
+
+home = {
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1
+}
+
+def roman2decimal(numeral):
+    symbols = list(numeral)
+    total = 0
+    for i in range(len(symbols)-1):
+        if home[symbols[i]] >= home[symbols[i+1]]:
+            total += home[symbols[i]]
+        else:
+            total += (home[symbols[i+1]] - home[symbols[i]])
+    return total
+
+print (roman2decimal("XIV"))
+print (roman2decimal("IV"))
+print (roman2decimal("XL"))
+print (roman2decimal("XXIV"))
