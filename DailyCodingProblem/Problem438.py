997ff1d1eb6faabdeee142f05e3b4679665b51d9
absognety <vikasch.1994@gmail.com>
2021-07-04 13:06:34 +0530

    Problem 438 ~ Problem 154

---
+++ DailyCodingProblem/Problem438.py
@@ -0,0 +1,20 @@
+"""
+Daily Coding Problem #438
+
+This problem was asked by Amazon.
+
+Implement a stack API using only a heap. A stack implements the 
+following methods:
+
+push(item), which adds an element to the stack
+pop(), which removes and returns the most recently added element 
+(or throws an error if there is nothing on the stack)
+Recall that a heap has the following operations:
+
+push(item), which adds a new key to the heap
+pop(), which removes and returns the max value of the heap
+
+Solution:
+    Duplicate of daily coding problem #154 - `implementStackWithHeap.py`
+
+"""
