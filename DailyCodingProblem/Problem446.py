207073fa714bc5130506052e7d959fd729ef41d4
absognety <vikasch.1994@gmail.com>
2021-07-11 10:37:53 +0530

    Is given integer N a power of 4

---
+++ DailyCodingProblem/Problem446.py
@@ -0,0 +1,22 @@
+"""
+Daily Coding Problem #446
+
+This problem was asked by Indeed.
+
+Given a 32-bit positive integer N, determine whether it is a power 
+of four in faster than O(log N) time.
+
+"""
+
+import math
+def isPowerOf4(N):
+    d = str(math.log(N,4))
+    if d.split(".")[1] == "0":
+        return True
+    else:
+        return False
+    
+print (isPowerOf4(256))
+print (isPowerOf4(128))
+print (isPowerOf4(1024))
+print (isPowerOf4(1023))
