5ab37bdedd6cd23e823a6c3544307e840b7762b2
absognety <vikasch.1994@gmail.com>
2021-08-08 20:01:10 +0530

    Added a problem statement

--- Hackerearth/Hexadecimal_Numbers.py
+++ Hackerearth/Hexadecimal_Numbers.py
@@ -5,6 +5,21 @@ name = input()                  # Reading input from STDIN
 print('Hi, %s.' % name)         # Writing output to STDOUT
 
 # Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+You are given a range [L,R] . You are required to find the number of integers  
+in the range such that GCD(X,F(X)) > 1 where F(X) is equal to the sum of 
+digits of X in its hexadecimal (or base 16) representation.
+
+For example, F(27) = 1 + B = 1 + 11 = 12 
+( 27 in hexadecimal is written as 1B). You are asked T such questions. 
+
+Input format
+
+The first line contains a positive integer T denoting the number of questions 
+that you are asked.
+Each of the next T lines contain two integers L and R denoting the range of 
+questions.
+
 '''
 # Hexadecimal synopsis - transformations
 lookup_dict = {"A":10,"B":11,"C":12,
