27ad15df737b6933eb863ce9ae3f8081fcb6c0a4
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2024-04-28 11:25:50 +0530

    SecondHighestSalary.sql from Leetcode

---
+++ LeetCode/SQL50/SecondHighestSalary.sql
@@ -0,0 +1,61 @@
+-- Table: Employee
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+ 
+
+-- Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- Example 2:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | null                |
+-- +---------------------+
+
+
+-- Solution:
+-- Write your PostgreSQL query statement below
+select max(ranked_employee.salary) SecondHighestSalary from 
+(select salary,
+    dense_rank() over (
+        order by salary desc
+    ) salary_rank
+from Employee
+) ranked_employee where ranked_employee.salary_rank = 2
