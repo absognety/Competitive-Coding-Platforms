8dfd4c0c73a8e160105916f216126eae048f3fe5
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2024-05-26 09:04:02 +0530

    ClassesMoreThan5Students.sql

---
+++ LeetCode/SQL50/ClassesMoreThan5Students.sql
@@ -0,0 +1,56 @@
+-- Table: Courses
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | student     | varchar |
+-- | class       | varchar |
+-- +-------------+---------+
+-- (student, class) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+-- Write a solution to find all the classes that have at least five students.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Example 1:
+
+-- Input: 
+-- Courses table:
+-- +---------+----------+
+-- | student | class    |
+-- +---------+----------+
+-- | A       | Math     |
+-- | B       | English  |
+-- | C       | Math     |
+-- | D       | Biology  |
+-- | E       | Math     |
+-- | F       | Computer |
+-- | G       | Math     |
+-- | H       | Math     |
+-- | I       | Math     |
+-- +---------+----------+
+-- Output: 
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+-- Explanation: 
+-- - Math has 6 students, so we include it.
+-- - English has 1 student, so we do not include it.
+-- - Biology has 1 student, so we do not include it.
+-- - Computer has 1 student, so we do not include it.
+
+
+-- Write your PostgreSQL query statement below
+-- Solution
+
+select C.class 
+from (
+    select class,
+        count(student) as student_count
+    from Courses group by class
+) C where C.student_count >= 5
