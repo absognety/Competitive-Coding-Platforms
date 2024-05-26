4fd510c36bce39f5eecb9890705d98c9df3bf39f
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2024-05-26 08:55:41 +0530

    LC medium ~ ProductSalesAnalysis-III.sql

---
+++ LeetCode/SQL50/ProductSalesAnalysis-III.sql
@@ -0,0 +1,76 @@
+-- Table: Sales
+
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- product_id is a foreign key (reference column) to Product table.
+-- Each row of this table shows a sale on the product product_id in a certain year.
+-- Note that the price is per unit.
+ 
+-- Table: Product
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the product name of each product.
+ 
+-- Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+-- Return the resulting table in any order.
+
+-- The result format is in the following example.
+
+-- Example 1:
+
+-- Input: 
+-- Sales table:
+-- +---------+------------+------+----------+-------+
+-- | sale_id | product_id | year | quantity | price |
+-- +---------+------------+------+----------+-------+ 
+-- | 1       | 100        | 2008 | 10       | 5000  |
+-- | 2       | 100        | 2009 | 12       | 5000  |
+-- | 7       | 200        | 2011 | 15       | 9000  |
+-- +---------+------------+------+----------+-------+
+-- Product table:
+-- +------------+--------------+
+-- | product_id | product_name |
+-- +------------+--------------+
+-- | 100        | Nokia        |
+-- | 200        | Apple        |
+-- | 300        | Samsung      |
+-- +------------+--------------+
+-- Output: 
+-- +------------+------------+----------+-------+
+-- | product_id | first_year | quantity | price |
+-- +------------+------------+----------+-------+ 
+-- | 100        | 2008       | 10       | 5000  |
+-- | 200        | 2011       | 15       | 9000  |
+-- +------------+------------+----------+-------+
+
+-- Write your PostgreSQL query statement below
+-- Solution
+
+with first_year as 
+(
+    select product_id, 
+           min(year) as first_year
+    from Sales group by product_id
+)
+select Sales.product_id,
+       first_year.first_year,
+       Sales.quantity,
+       Sales.price
+from Sales inner join first_year 
+on (Sales.product_id = first_year.product_id 
+   and Sales.year = first_year.first_year)
