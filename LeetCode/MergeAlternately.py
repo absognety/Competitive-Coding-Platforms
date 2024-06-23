d692e295a1eaeb0da7d1d526d60fedc76101501e
Vikas Chitturi <csubramanya@athenahealth.com>
2024-06-23 18:49:06 +0530

    Merge Strings/lists alternately

--- LeetCode/MergeAlternately.py
+++ LeetCode/MergeAlternately.py
@@ -1,4 +1,6 @@
-# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
+# starting with word1. If a string is longer than the other, append the additional letters 
+# onto the end of the merged string.
 
 # Return the merged string.
 
