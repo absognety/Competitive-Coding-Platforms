41b7db194f49e030f8ece006172f551b2eacc7e5
absognety <vikasch.1994@gmail.com>
2024-11-02 09:44:12 +0530

    Formatting

--- LeetCode/ProgrammingInGo/main.go
+++ LeetCode/ProgrammingInGo/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"ProgrammingInGo/MinMovesToSeatEveryone"
-	"ProgrammingInGo/MergeAlternately"
-	"ProgrammingInGo/gcd"
 	"ProgrammingInGo/KidsWithGreatestCandies"
+	"ProgrammingInGo/MergeAlternately"
+	"ProgrammingInGo/MinMovesToSeatEveryone"
 	"ProgrammingInGo/ReverseWords"
+	"ProgrammingInGo/gcd"
 	"ProgrammingInGo/productofelements"
+	"fmt"
 )
 
 func main() {
 	// test the function
 	// Use Exported Types to have visibility into functions from different package
-	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{3,1,5}, []int{2,7,4}))
-	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{4,1,5,9}, []int{1,3,2,6}))
-	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{2,2,6,6}, []int{1,3,2,6}))
+	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{3, 1, 5}, []int{2, 7, 4}))
+	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{4, 1, 5, 9}, []int{1, 3, 2, 6}))
+	fmt.Println(MinMovesToSeatEveryone.MinMovesToSeat([]int{2, 2, 6, 6}, []int{1, 3, 2, 6}))
 
 	// test the function
 	fmt.Println(MergeAlternately.MergeAlternate("abc", "pqr"))
@@ -28,8 +28,8 @@ func main() {
 	fmt.Println(gcd.GCDofStrings("LEET", "CODE"))
 
 	// test the function
-	fmt.Println(KidsWithGreatestCandies.KidsWithCandies([]int{2,3,5,1,3}, 3))
-	fmt.Println(KidsWithGreatestCandies.KidsWithCandies([]int{4,2,1,1,2}, 1))
+	fmt.Println(KidsWithGreatestCandies.KidsWithCandies([]int{2, 3, 5, 1, 3}, 3))
+	fmt.Println(KidsWithGreatestCandies.KidsWithCandies([]int{4, 2, 1, 1, 2}, 1))
 
 	// test the function
 	fmt.Println(ReverseWords.ReverseTokens("the sky is blue"))
@@ -37,8 +37,8 @@ func main() {
 	fmt.Println(ReverseWords.ReverseTokens("a good   example"))
 
 	// test the function
-	fmt.Println(productofelements.ProductExceptSelf([]int{1,2,3,4}))
+	fmt.Println(productofelements.ProductExceptSelf([]int{1, 2, 3, 4}))
 	fmt.Println(productofelements.ProductExceptSelf([]int{-1, 1, 0, -3, 3}))
-	fmt.Println(productofelements.ProductExceptSelf_Optimized([]int{1,2,3,4}))
+	fmt.Println(productofelements.ProductExceptSelf_Optimized([]int{1, 2, 3, 4}))
 	fmt.Println(productofelements.ProductExceptSelf_Optimized([]int{-1, 1, 0, -3, 3}))
 }
