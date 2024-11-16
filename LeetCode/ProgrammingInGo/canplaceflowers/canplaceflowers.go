45feb14bf367828d18b41caa57a253c0e944ebfe
absognety <vikasch.1994@gmail.com>
2024-11-16 09:57:35 +0530

    Can we plant flowers with given locations ~ Leetcode 75

--- LeetCode/ProgrammingInGo/canplaceflowers/canplaceflowers.go
+++ LeetCode/ProgrammingInGo/canplaceflowers/canplaceflowers.go
@@ -1,5 +1,7 @@
 /*
 
+Leetcode 75 Medium
+
 You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
 
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
