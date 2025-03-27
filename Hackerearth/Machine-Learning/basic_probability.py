004a5768fdf9bbdabe3bf696c15e9e5718922395
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2025-03-27 11:43:30 +0530

    comments to docstring

--- Hackerearth/Machine-Learning/basic_probability.py
+++ Hackerearth/Machine-Learning/basic_probability.py
@@ -1,23 +1,25 @@
-# Mike wants to go fishing this weekend to nearby lake. His neighbour 
-# Alice (is the one Mike was hoping to ask out since long time) is also planing to go to the same spot for fishing this weekend. 
-# The probability that it will rain this weekend is p1.There are two possible ways to reach the fishing spot (bus or train). 
-# The probability that Mike will take the bus is Pmb and that Alice will take the bus is Pab. 
-# Travel plans of both are independent of each other and rain.
+'''
+Mike wants to go fishing this weekend to nearby lake. His neighbour 
+Alice (is the one Mike was hoping to ask out since long time) is also planing to go to the same spot for fishing this weekend. 
+The probability that it will rain this weekend is p1.There are two possible ways to reach the fishing spot (bus or train). 
+The probability that Mike will take the bus is Pmb and that Alice will take the bus is Pab. 
+Travel plans of both are independent of each other and rain.
 
-# What is the probability Prs that Mike and Alice meet each other only (should not meet in bus or train) 
-# in a romantic setup (on a lake in rain)?
+What is the probability Prs that Mike and Alice meet each other only (should not meet in bus or train) 
+in a romantic setup (on a lake in rain)?
 
-# Input format
+Input format
 
-# FIrst line: Pmb
+FIrst line: Pmb
 
-# Second line: Pab
+Second line: Pab
 
-# Third line: p1
+Third line: p1
 
-# Output format
+Output format
 
-# Prs, rounded up to six decimal places.
+Prs, rounded up to six decimal places.
+'''
 
 import math
 
