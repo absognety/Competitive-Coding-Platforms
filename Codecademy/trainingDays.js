3a5b0a58978378e37c93a8cf494961bf168df057
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2026-03-14 22:13:26 +0530

    Fix string quotes and update comments in trainingDays.js
    Scoping example

---
+++ Codecademy/trainingDays.js
@@ -0,0 +1,46 @@
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return "Marathon";
+  } else if (random === 1) {
+    return "Triathlon";
+  } else if (random === 2) {
+    return "Pentathlon";
+  }
+};
+
+// The scope of `days` is too tight
+const getTrainingDays = (event) => {
+  let days;
+  if (event === "Marathon") {
+    days = 50;
+  } else if (event === "Triathlon") {
+    days = 100;
+  } else if (event === "Pentathlon") {
+    days = 200;
+  }
+  return days;
+};
+
+// The scope of `name` is too tight
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event1 = getRandEvent();
+const days1 = getTrainingDays(event1);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime
+const name1 = "Nala";
+logEvent(name1, event1);
+logTime(name1, days1);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = "Warren";
+
+logEvent(name2, event2);
+logTime(name2, days2);
