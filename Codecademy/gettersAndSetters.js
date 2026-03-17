b18e1359e48a95e40ad88506690396d5984f8376
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2026-03-17 16:20:01 +0530

    Implement getters and setters for robot sensors

---
+++ Codecademy/gettersAndSetters.js
@@ -0,0 +1,23 @@
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors() {
+    if (typeof this._numOfSensors === "number") {
+      return this._numOfSensors;
+    } else {
+      return "Sensors are currently down.";
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0");
+    }
+  },
+};
+
+console.log(robot.numOfSensors);
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
