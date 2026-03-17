9d3e246fdbc249d1e85c8ffb43f9f58d74fb94f2
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2026-03-17 17:38:31 +0530

    Implement robot object with Object methods usage
    Add robot object and demonstrate Object methods.

---
+++ Codecademy/BuiltInObjectMethods.js
@@ -0,0 +1,26 @@
+const robot = {
+  model: "SAL-1000",
+  mobile: true,
+  sentient: false,
+  armor: "Steel-plated",
+  energyLevel: 75,
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot);
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({}, robot, {
+  laserBlaster: true,
+  voiceRecognition: true,
+});
+
+console.log(newRobot);
+console.log(robot);
