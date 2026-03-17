bd7e3df2d0e16036374e103422fc714086b95efb
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2026-03-17 16:33:05 +0530

    Add robot object with functionality methods

---
+++ Codecademy/destructuring.js
@@ -0,0 +1,16 @@
+const robot = {
+  model: "1E78V2",
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log("Beep Boop");
+    },
+    fireLaser() {
+      console.log("Pew Pew");
+    },
+  },
+};
+
+const { functionality } = robot;
+functionality.beep();
+functionality.fireLaser();
