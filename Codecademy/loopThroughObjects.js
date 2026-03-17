5ba175b11381c8e7e35917d9c0eaafcb234f0266
Vikas Chitturi - Open Source Contributor <vikasch.1994@gmail.com>
2026-03-17 15:25:55 +0530

    Create spaceship object and loop through crew members
    Added a spaceship object with crew members and their details. Implemented loops to display crew member names and degrees.

---
+++ Codecademy/loopThroughObjects.js
@@ -0,0 +1,43 @@
+let spaceship = {
+  crew: {
+    captain: {
+      name: "Lily",
+      degree: "Computer Engineering",
+      cheerTeam() {
+        console.log("You got this!");
+      },
+    },
+    "chief officer": {
+      name: "Dan",
+      degree: "Aerospace Engineering",
+      agree() {
+        console.log("I agree, captain!");
+      },
+    },
+    medic: {
+      name: "Clementine",
+      degree: "Physics",
+      announce() {
+        console.log(`Jets on!`);
+      },
+    },
+    translator: {
+      name: "Shauna",
+      degree: "Conservation Science",
+      powerFuel() {
+        console.log("The tank is full!");
+      },
+    },
+  },
+};
+
+// Write your code below
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+
+for (let crewMember in spaceship.crew) {
+  console.log(
+    `${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`
+  );
+}
